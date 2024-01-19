7ec9989affe5d3d4e87f2327497eebbb5ff9f79d
Filche <filche_merigold@mail.ru>
2024-01-20 00:24:09 +1000

    Added true tickets.json file commit

--- src/main/java/Ticket.java
+++ src/main/java/Ticket.java
@@ -14,31 +14,30 @@ import java.time.LocalTime;
 @ToString
 public class Ticket {
 
+    @JsonProperty("origin")
+    private String origin;
     @JsonProperty("origin_name")
     private String originName;
-
+    @JsonProperty("destination")
+    private String destination;
     @JsonProperty("destination_name")
     private String destinationName;
-
     @JsonProperty("departure_date")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd.MM.yy")
     private LocalDate departureDate;
-
     @JsonProperty("departure_time")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "H:mm")
     private LocalTime departureTime;
-
     @JsonProperty("arrival_date")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd.MM.yy")
     private LocalDate arrivalDate;
-
     @JsonProperty("arrival_time")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "H:mm")
     private LocalTime arrivalTime;
-
     @JsonProperty("carrier")
     private String carrier;
-
+    @JsonProperty("stops")
+    private int stops;
     @JsonProperty("price")
     private int price;
 }
